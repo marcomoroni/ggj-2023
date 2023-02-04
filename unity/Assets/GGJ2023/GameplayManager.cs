e8b28c2cacda732dfae3c18dd73a8e05486dfef0
Marco Moroni <marcomoroni394@gmail.com>
2023-02-04 20:24:15 +0000

    more more

--- unity/Assets/GGJ2023/GameplayManager.cs
+++ unity/Assets/GGJ2023/GameplayManager.cs
@@ -4,6 +4,15 @@ namespace GGJ2023
 {
     public class GameplayManager : MonoBehaviour
     {
+        private abstract class State
+        {
+            public virtual void OnEnter() { }
+            public virtual void OnExit() { }
+        }
 
+        private class MenuState
+        {
+
+        }
     }
 }
--- unity/Assets/GGJ2023/GameplayTurnip.cs
+++ unity/Assets/GGJ2023/GameplayTurnip.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GGJ2023
@@ -7,7 +8,11 @@ namespace GGJ2023
         [SerializeField]
         private new Rigidbody rigidbody;
         [SerializeField]
-        private int stepsToComplete = 1;
+        private Transform height;
+
+        private int stepsRequired;
+        private bool listenForInput = false;
+        private int stepsSoFar = -1;
 
         private void Start()
         {
@@ -16,14 +21,30 @@ namespace GGJ2023
             rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
         }
 
+        public void Initialise(int stepsRequired)
+        {
+            this.stepsRequired = stepsRequired;
+        }
+
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            Debug.Log(Input.touchCount);
+            //if (listenForInput)
             {
-                rigidbody.isKinematic = false;
-                rigidbody.AddForce(new Vector3(Random.Range(-2, 2), Random.Range(2, 4), Random.Range(-2, 2)), ForceMode.Impulse);
-                rigidbody.AddTorque(new Vector3(Random.Range(-2, 2), Random.Range(2, 2), Random.Range(-2, 2)), ForceMode.Impulse);
+                if (Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+                {
+                    stepsSoFar++;
+
+                    rigidbody.isKinematic = false;
+                    rigidbody.AddForce(new Vector3(Random.Range(-2, 2), Random.Range(2, 4), Random.Range(-2, 2)), ForceMode.Impulse);
+                    rigidbody.AddTorque(new Vector3(Random.Range(-2, 2), Random.Range(2, 2), Random.Range(-2, 2)), ForceMode.Impulse);
+                }
             }
+        }
+
+        public void ListenForInput()
+        {
+            listenForInput = true;
         }
     }
 }
