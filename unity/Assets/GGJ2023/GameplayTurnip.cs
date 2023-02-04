6d47cfd214b6cf382cf062919e7ada4a358c5fdd
Marco Moroni <marcomoroni394@gmail.com>
2023-02-04 17:26:10 +0000

    more progress

--- unity/Assets/GGJ2023/GameplayTurnip.cs
+++ unity/Assets/GGJ2023/GameplayTurnip.cs
@@ -4,6 +4,26 @@ namespace GGJ2023
 {
     public class GameplayTurnip : MonoBehaviour
     {
+        [SerializeField]
+        private new Rigidbody rigidbody;
+        [SerializeField]
+        private int stepsToComplete = 1;
 
+        private void Start()
+        {
+            rigidbody.isKinematic = true;
+            rigidbody.angularDrag = 5;
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidbody.isKinematic = false;
+                rigidbody.AddForce(new Vector3(Random.Range(-2, 2), Random.Range(2, 4), Random.Range(-2, 2)), ForceMode.Impulse);
+                rigidbody.AddTorque(new Vector3(Random.Range(-2, 2), Random.Range(2, 2), Random.Range(-2, 2)), ForceMode.Impulse);
+            }
+        }
     }
 }
